94964c2239aeea971cba35fcfad8ec81378a4a4c
Yaolin Ge <geyaolin@gmail.com>
2024-02-07 21:52:05 +0100

    add new notes for learning javascript

--- practice/ProgrammingWithJavascript/w1.js
+++ practice/ProgrammingWithJavascript/w1.js
@@ -157,3 +157,41 @@ for (var i = 0; i < cubes.length; i++)
     var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple; "; 
     console.log("%c" + cubes[i], styles); 
 }
+
+for (var i = 1; i <= 10; i++)
+{
+    if (i == 1)
+    {
+        console.log("Gold medal. "); 
+    }
+    else if (i == 2)
+    {
+        console.log("Silver medal. "); 
+    }
+    else if (i == 3)
+    {
+        console.log("Bronze medal. "); 
+    }
+    else 
+    {
+        console.log(i); 
+    }
+}
+
+for (var i = 1; i < 11; i++)
+{
+    switch (i)
+    {
+        case 1:
+            console.log("Gold medal. ");
+            break;
+        case 2:
+            console.log("Silver medal. ");
+            break;
+        case 3:
+            console.log("Bronze medal. ");
+            break;
+        default:
+            console.log(i); 
+    }
+}
