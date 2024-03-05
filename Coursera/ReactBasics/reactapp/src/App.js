ad49b6aa32c53de15ab87c1275486414ddfa6d77
Yaolin Ge <geyaolin@gmail.com>
2024-03-05 09:16:52 +0100

    prepare capstone project

--- Coursera/ReactBasics/reactapp/src/App.js
+++ Coursera/ReactBasics/reactapp/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 // Import all the modules from w1, w2, w3 and project and reference them in the return statement, with a button I can switch between different components. 
 
+
 export default function App() {
   return(
     <>
